ba9947390ae9a23011788ea2f4fdf1550a7e6dc9
WoobinHwang <dnqls0326@naver.com>
2022-07-06 15:35:14 +0900

    페이지버튼 + css추가

--- VamPa_Board(Oracle)/src/main/java/com/vam/controller/BoardController.java
+++ VamPa_Board(Oracle)/src/main/java/com/vam/controller/BoardController.java
@@ -12,95 +12,121 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.vam.model.BoardVO;
 import com.vam.model.Criteria;
+import com.vam.model.PageMakerDTO;
 import com.vam.service.BoardService;
-
-import lombok.Data;
 
 @Controller
 @RequestMapping("/board/*")
 public class BoardController {
-	
+
 	private static final Logger log = LoggerFactory.getLogger(BoardController.class);
 
 	@Autowired
-    private BoardService bservice;
-	
+	private BoardService bservice;
+
+	/* 게시판 목록 페이지 접속(페이징 적용) */
 	@GetMapping("/list")
 	public void boardListGET(Model model, Criteria cri) {
+		
 		log.info("게시판 목록 페이지 진입");
-		
+
 		model.addAttribute("list", bservice.getListPaging(cri));
-		
+
+		int total = bservice.getTotal();
+        
+        PageMakerDTO pageMake = new PageMakerDTO(cri, total);
+        
+        model.addAttribute("pageMaker", pageMake);
+        
 //		/WEB-INF/views/list.jsp
 	}
 	
+	
+
 	@GetMapping("/enroll")
 	public void boardEnrollGET() {
 		log.info("게시판 등록 페이지 진입");
 	}
-	
+
 	@GetMapping("/custom")
 	public void boardCustomGET() {
 		log.info("커스텀 진입");
-	
+
 	}
-	
+
 	/* 게시판 등록 */
-    @PostMapping("/enroll")
-    public String boardEnrollPOST(BoardVO board, RedirectAttributes rttr) {
-        System.out.println("타겟");
-    	System.out.println(rttr);
-        log.info("BoardVO : " + board);
-        
-        bservice.enroll(board);
-        
-        rttr.addFlashAttribute("result", "enroll success");
-        
-        return "redirect:/board/list";
-        
-    }
-    
-    
-    /* 게시글 상세조회 */
-    /* GET요청은 페이지 이동이 거듭되는동안 이전 페이지들의 요청정보를 기억하고있어야한다. */
+	@PostMapping("/enroll")
+	public String boardEnrollPOST(BoardVO board, RedirectAttributes rttr) {
+		System.out.println("타겟");
+		System.out.println(rttr);
+		log.info("BoardVO : " + board);
+
+		bservice.enroll(board);
+
+		rttr.addFlashAttribute("result", "enroll success");
+
+		return "redirect:/board/list";
+
+	}
+
+	/* 게시글 상세조회 */
+	/* GET요청은 페이지 이동이 거듭되는동안 이전 페이지들의 요청정보를 기억하고있어야한다. */
 	/* URL에 파라미터가 누적되어 전달되는데 이런 기법을 URL Rewrite처리라고 한다. */
-    @GetMapping("/get")
-    public void boardGetPageGET(int bno, Model model) {
-        
-        model.addAttribute("pageInfo", bservice.getPage(bno));
-        
-    }
-    
-    /* 수정 페이지 이동 */
-    @GetMapping("/modify")
-    public void boardModifyGET(int bno, Model model) {
-        
-        model.addAttribute("pageInfo", bservice.getPage(bno));
-        
-    }
-    
-    /* 글 수정 */
-    @PostMapping("/modify")
-    public String boardModifyPOST(BoardVO board, RedirectAttributes rttr) {
-        
-        bservice.modify(board);
-        
-        rttr.addFlashAttribute("result", "modify success");
-        
-        return "redirect:/board/list";
-        
-    }
-    
-    /* 글 삭제 */
-    @PostMapping("/delete")
-    public String boardDeletePOST(int bno, RedirectAttributes rttr) {
-        
-        bservice.delete(bno);
-        
-        rttr.addFlashAttribute("result", "delete success");
-        
-        return "redirect:/board/list";
-        
-    }
-    
+	@GetMapping("/get")
+	public void boardGetPageGET(int bno, Model model) {
+
+		model.addAttribute("pageInfo", bservice.getPage(bno));
+
+	}
+
+	/* 수정 페이지 이동 */
+	@GetMapping("/modify")
+	public void boardModifyGET(int bno, Model model) {
+
+		model.addAttribute("pageInfo", bservice.getPage(bno));
+
+	}
+
+	/* 글 수정 */
+	@PostMapping("/modify")
+	public String boardModifyPOST(BoardVO board, RedirectAttributes rttr) {
+
+		bservice.modify(board);
+
+		rttr.addFlashAttribute("result", "modify success");
+
+		return "redirect:/board/list";
+
+	}
+
+	/* 글 삭제 */
+	@PostMapping("/delete")
+	public String boardDeletePOST(int bno, RedirectAttributes rttr) {
+
+		bservice.delete(bno);
+
+		rttr.addFlashAttribute("result", "delete success");
+
+		return "redirect:/board/list";
+
+	}
+
+	/*
+	 * 게시판 목록 페이지 접속(페이징 적용)
+	 * 
+	 * @GetMapping("/list") public void boardListGET(Model model, Criteria cri) {
+	 * 
+	 * log.info("boardListGET");
+	 * 
+	 * model.addAttribute("list", bservice.getListPaging(cri));
+	 * 
+	 * int total = bservice.getTotal();
+	 * 
+	 * PageMakerDTO pageMake = new PageMakerDTO(cri, total);
+	 * 
+	 * model.addAttribute("pageMaker", pageMake);
+	 * 
+	 * }
+	 */
+
 }
--- VamPa_Board(Oracle)/src/test/java/com/vam/mapper/BoardMapperTests.java
+++ VamPa_Board(Oracle)/src/test/java/com/vam/mapper/BoardMapperTests.java
@@ -98,4 +98,14 @@ public class BoardMapperTests {
          list.forEach(board -> log.info("" + board));
      }
      
+     /* 게시판 개수 찾기 테스트 */
+     @Test
+     public void testGetTotal() {
+                          
+         int total = mapper.getTotal();
+         log.info("등록된 총 글의 수 : " + total);
+         
+         
+     }
+     
 }
